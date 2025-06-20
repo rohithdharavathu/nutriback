09a795201f035aa2924b18d97a2128733104e5d6
rohithdharavathu <90998851+rohithdharavathu@users.noreply.github.com>
2025-06-20 14:53:13 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,4 +7,8 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to the FastAPI service!"}
 
+@app.head("/")
+def head_root():
+    return Response(status_code=200)
+
 app.include_router(user_router, prefix="/user", tags=["user"])
